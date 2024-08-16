e23fdf79d5c76be15ea395fb346094ef5648f7b3
SOHEE LEE <163387330+0isohee@users.noreply.github.com>
2024-08-16 16:25:47 +0900

    Title: 왕실의 기사 대결, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240816/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240816/왕실의 기사 대결/royal-knight-duel.cpp
@@ -16,40 +16,13 @@ vector<vector<pair<int,int>>> visited;
 vector<Person> peopleList;
 vector<pair<int,int>> orders;
 
-void printMap() {
-
-    /*
-    for (int i=0; i<L; i++) {
-        for (int j=0; j<L; j++) {
-            cout << map[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    for (int i=0; i<N; i++) {
-        Info info = peopleList[i];
-        cout << info.r << " " << info.c << " " << info.h << " " << info.w  << " " << info.k << "\n";
-    }
-    for (int i=0; i<Q; i++) {
-        cout << orders[i].first << " " << orders[i].second << "\n";
-    }   
-    */
-
-   for (int i=0; i<L; i++) {
-        for (int j=0; j<L; j++) {
-            cout << visited[i][j].first << " " << visited[i][j].second << " ";
-        }
-        cout << "\n";
-    }
-}
-
 bool isRange(int x, int y) {
-    return x >=0 && x < L && y >= 0 && y < L;
+    return x >= 0 && x < L && y >= 0 && y < L;
 }
 
 void init() {
-
-    map.resize(L, vector<int>(L,0));
-    visited.resize(40, vector<pair<int,int>>(40, {0,0}));
+    map.resize(L, vector<int>(L, 0));
+    visited.resize(L, vector<pair<int,int>>(L));
     peopleList.resize(N);
     orders.resize(Q);
 
@@ -60,7 +33,7 @@ void init() {
     }
     for (int i=0; i<N; i++) {
         Person p;
-        p.num = i+1;
+        p.num = i + 1;
         cin >> p.r >> p.c >> p.h >> p.w >> p.k;
         p.ok = p.k;
         p.isdead = false;
@@ -69,28 +42,28 @@ void init() {
     for (int i=0; i<Q; i++) {
         int num, dir;
         cin >> num >> dir;
-        orders[i] = {num,dir};
+        orders[i] = {num, dir};
     }
 
-    //vistied에 현재 상태 표시
+    // 현재 상태를 visited에 표시
     for (int i=0; i<N; i++) {
         Person p = peopleList[i];
-
         int sx = p.r - 1;
         int sy = p.c - 1;
-        for (int x=sx; x<sx+p.h; x++){
-            for (int y=sy; y<sy+p.w; y++) {
-                if (!isRange(x, y)) continue;
-                visited[x][y].first = p.num;
-                visited[x][y].second = p.k;
+        for (int x = sx; x < sx + p.h; x++) {
+            for (int y = sy; y < sy + p.w; y++) {
+                if (isRange(x, y)) { // 경계 체크 추가
+                    visited[x][y].first = p.num;
+                    visited[x][y].second = p.k;
+                }
             }
         }
     }
 }
 
-int checkIdx(int n){
-    for (int i=0; i<peopleList.size(); i++) {
-        if (!peopleList[i].isdead &&peopleList[i].num == n) {
+int checkIdx(int n) {
+    for (int i = 0; i < peopleList.size(); i++) {
+        if (!peopleList[i].isdead && peopleList[i].num == n) {
             return i;
         }
     }
@@ -98,29 +71,32 @@ int checkIdx(int n){
 }
 
 void movePerson(int orin, int idx, Person p, int d) {
-
     if (p.isdead) return;
+
     int sx = p.r - 1;
     int sy = p.c - 1;
-    for (int x=sx; x<sx+p.h; x++){
-        for (int y=sy; y<sy+p.w; y++) {
-            if(!isRange(x,y)) continue;
-            visited[x][y].first = 0;
-            visited[x][y].second = 0;
+
+    // 기존 위치 비우기
+    for (int x = sx; x < sx + p.h; x++) {
+        for (int y = sy; y < sy + p.w; y++) {
+            if (isRange(x, y)) { // 경계 체크 추가
+                visited[x][y].first = 0;
+                visited[x][y].second = 0;
+            }
         }
     }
 
+    // 이동
     p.r += dx[d];
     p.c += dy[d];
-
     sx = p.r - 1;
     sy = p.c - 1;
-    
-    if (orin != idx){
-        //함정 있으면 데미지 적용
-        for (int x=sx; x<sx+p.h; x++){
-            for (int y=sy; y<sy+p.w; y++) {
-                if (map[x][y] == 1) {
+
+    // 함정 데미지 적용
+    if (orin != idx) {
+        for (int x = sx; x < sx + p.h; x++) {
+            for (int y = sy; y < sy + p.w; y++) {
+                if (isRange(x, y) && map[x][y] == 1) { // 경계 체크 추가
                     p.k -= 1;
                 }
             }
@@ -131,10 +107,13 @@ void movePerson(int orin, int idx, Person p, int d) {
         p.isdead = true;
     }
 
-    for (int x=sx; x<sx+p.h; x++){
-        for (int y=sy; y<sy+p.w; y++) {
-            visited[x][y].first = p.num;
-            visited[x][y].second = p.k;
+    // 새로운 위치에 업데이트
+    for (int x = sx; x < sx + p.h; x++) {
+        for (int y = sy; y < sy + p.w; y++) {
+            if (isRange(x, y)) { // 경계 체크 추가
+                visited[x][y].first = p.num;
+                visited[x][y].second = p.k;
+            }
         }
     }
 
@@ -142,22 +121,19 @@ void movePerson(int orin, int idx, Person p, int d) {
 }
 
 bool isCanMove(int orin, Person p, int d) {
-
     int nx = p.r + dx[d] - 1;
     int ny = p.c + dy[d] - 1;
 
-    for (int x=nx; x<nx+p.h; x++){
-        for (int y=ny; y<ny+p.w; y++) {
-            //범위 벗어나거나 벽이면
-            if (!isRange(x,y)){
+    for (int x = nx; x < nx + p.h; x++) {
+        for (int y = ny; y < ny + p.w; y++) {
+            // 범위 벗어나거나 벽이면 이동 불가
+            if (!isRange(x, y)) {
                 return false;
-            } 
-            else if (map[x][y] == 2)  {
+            } else if (map[x][y] == 2) {
                 return false;
-            } 
-            //다른 기사 있을 때 다른 기사 연쇄 이동
-            else if (isRange(x,y) && visited[x][y].first > 0 && visited[x][y].first != p.num) {
-                
+            }
+            // 다른 기사가 있으면 연쇄 이동 처리
+            else if (visited[x][y].first > 0 && visited[x][y].first != p.num) {
                 int idx = checkIdx(visited[x][y].first);
                 if (idx == -1) continue;
                 if (isCanMove(orin, peopleList[idx], d)) {
@@ -172,23 +148,19 @@ bool isCanMove(int orin, Person p, int d) {
 }
 
 void gameStart() {
-
-    for (int i=0; i<Q; i++) {
+    for (int i = 0; i < Q; i++) {
         int num = orders[i].first - 1;
         int dir = orders[i].second;
 
-        if (!peopleList[num].isdead && isCanMove(i, peopleList[num], dir)) {
-            movePerson(i, i, peopleList[num], dir);
+        if (!peopleList[num].isdead && isCanMove(num, peopleList[num], dir)) {
+            movePerson(num, num, peopleList[num], dir);
         }
-
-        // printMap();
-        // cout << "\n";
     }
 }
 
 int getDamage() {
     int ans = 0;
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
         if (peopleList[i].isdead) continue;
         ans += (peopleList[i].ok - peopleList[i].k);
     }
@@ -196,7 +168,6 @@ int getDamage() {
 }
 
 int main() {
-
     cin.tie(NULL);
     cout.tie(NULL);
     ios::sync_with_stdio(false);
